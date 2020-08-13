9656fa1969489b01aa19c78d74ec36a405b135cd
Jayson Presto <jaysonpresto.iprog21@gmail.com>
2020-08-13 18:47:21 +0800

    Generate New Parking Lots

--- app/controllers/parking_lots_controller.rb
+++ app/controllers/parking_lots_controller.rb
@@ -1,5 +1,10 @@
 class ParkingLotsController < ApplicationController
-  def new
-    
+  def new; end
+  def create
+    total_parking_lots = params[:total_parking_lots]
+    for i in 1..total_parking_lots.to_i
+      ParkingLot.create
+    end
+    redirect_to request.referrer, notice: "New Parking Lots Has Been Successfully Created"
   end
 end
