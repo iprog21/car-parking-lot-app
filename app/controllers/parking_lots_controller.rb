2237788d9ad8cca8aee6a223abef89a0fff6f419
Jayson Presto <jaysonpresto.iprog21@gmail.com>
2020-08-13 19:01:00 +0800

    Build Car Parking Lots List

--- app/controllers/parking_lots_controller.rb
+++ app/controllers/parking_lots_controller.rb
@@ -5,6 +5,6 @@ class ParkingLotsController < ApplicationController
     for i in 1..total_parking_lots.to_i
       ParkingLot.create
     end
-    redirect_to request.referrer, notice: "New Parking Lots Has Been Successfully Created"
+    redirect_to root_path, notice: "New Parking Lots Has Been Successfully Created"
   end
 end
